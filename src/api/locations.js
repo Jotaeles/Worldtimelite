0d32806579bd991ba5aa0c50d122a620f043dd3e
jotaeles <jl_sandoval_salas@hotmail.com>
2022-01-25 21:01:07 -0600

    add https to urls

--- src/api/locations.js
+++ src/api/locations.js
@@ -1,6 +1,6 @@
 export const getTimezones = async () => {
   try {
-    let url = `http://worldtimeapi.org/api/timezone`;
+    let url = `https://worldtimeapi.org/api/timezone`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -11,7 +11,7 @@ export const getTimezones = async () => {
 
 export const getLocationData = async (location) => {
   try {
-    let url = `http://worldtimeapi.org/api/timezone/${location}`;
+    let url = `https://worldtimeapi.org/api/timezone/${location}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
